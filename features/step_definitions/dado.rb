eb2f6c8b1b593b6c5b7d4efaf8a28702f63edb16
Lucas Barretto <lucasbarrettofontes@hotmail.com>
2020-04-10 16:23:02 -0300

    'token'

--- features/step_definitions/dado.rb
+++ features/step_definitions/dado.rb
@@ -2,7 +2,7 @@ Dado("que eu tenha acesso a url da Spotify e solicito os dados do user") do
   $response = HTTParty.get('https://api.spotify.com/v1/me',
      headers: 
      {
-          "Authorization" => 'Bearer BQCKO4yPxpxMrnDpaakeCmweaeKP9aytxQvlrosT55Lea3_7wy09XDoGVNZRAUXytNBxfQs-K9V6exEX7GFUCaedHsVezlWfGSduw6rjhe9KnOYE3vfWFzQOuMYLBFWLsE21UfGwaX60ANEmC5B0NBEk5n8851OqfuKSjkhOs_2U',
+          "Authorization" => 'Bearer BQCKO4yPxpxMrnDpaakeCmweaeKP9aytxQvlrosT55Lea3_7wy09XDoGVNZRAUXytNBxfQs-K9V6exEX7GFUCaedHsVezlWfGSduw6rjhe9KnOYE3vfWFzQOuMYLBFWLsE21UfGwaX60ANEmC5B0NBEk5n8851OqfuKSjkhOs_2',
           "Content-Type" => 'application/json'
      }
   )
